d75463419ebfbb13d103bf2af0961147cd119961
Keanu Morris <keanu.morris@gmail.com>
2021-10-15 17:14:32 -0700

    print date

--- src/edu/pcc/cis/helloworld/HelloWorld.java
+++ src/edu/pcc/cis/helloworld/HelloWorld.java
@@ -1,5 +1,7 @@
 package edu.pcc.cis.helloworld;
 
+import java.util.Date;
+
 /**
  * Created by keanu-morris on 10/15/2021
  */
@@ -7,5 +9,6 @@ package edu.pcc.cis.helloworld;
 public class HelloWorld {
     public static void main(String[] args) {
         System.out.println("Hello, World!");
+        System.out.println(new Date());
     }
 }
