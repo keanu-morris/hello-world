58f83469037e227cdd870de8d1fe8cb9e7dfcc99
Keanu Morris <keanu.morris@gmail.com>
2021-10-15 17:10:19 -0700

    needed to commit again

--- src/edu/pcc/cis/helloworld/HelloWorld.java
+++ src/edu/pcc/cis/helloworld/HelloWorld.java
@@ -1,5 +1,11 @@
 package edu.pcc.cis.helloworld;
 
+/**
+ * Created by keanu-morris on 10/15/2021
+ */
 
 public class HelloWorld {
+    public static void main(String[] args) {
+        System.out.println("Hello, World!");
+    }
 }
